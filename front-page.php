d754dcc83f395d31ca6ec5ccc09d41de05db4684
Dave Redfern <dave@daveredfern.com>
2016-07-25 18:30:14 +0100

    Fixing date issue

--- front-page.php
+++ front-page.php
@@ -26,7 +26,7 @@
 		while( $latestarticles->have_posts() ) {
 			$latestarticles->the_post();
 		?>
-			<li><strong><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></strong> &mdash; <?php the_time('M S'); ?></li>
+			<li><strong><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></strong> &mdash; <?php the_time('M j'); ?></li>
 		<?php
 		}
 		wp_reset_query();
