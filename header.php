6ddb74fb2dd90da2ba2a8985c0444d3f08ff0c88
Dave Redfern <dave@daveredfern.com>
2016-08-08 17:53:02 +0100

    Adding favicon

--- header.php
+++ header.php
@@ -22,6 +22,13 @@
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/build/vendor/favicons/apple-touch-icon.png">
+	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/build/vendor/favicons/favicon-32x32.png" sizes="32x32">
+	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/build/vendor/favicons/favicon-16x16.png" sizes="16x16">
+	<link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/build/vendor/favicons/manifest.json">
+	<link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/build/vendor/favicons/safari-pinned-tab.svg" color="#5bbad5">
+	<meta name="theme-color" content="#ffffff">
+
 	<style>
 		<?php echo file_get_contents(get_template_directory() . '/build/css/styles.css'); ?>
 		<?php
