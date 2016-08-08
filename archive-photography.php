0b2c8e0a76881ab7743f5278ef0dfb5e79727bd0
Dave Redfern <dave@daveredfern.com>
2016-08-08 23:17:06 +0100

    Adding lazyloader to photography section

--- archive-photography.php
+++ archive-photography.php
@@ -6,7 +6,9 @@
         <p>All my photos are free to use, released under the <a href="https://creativecommons.org/publicdomain/zero/1.0/">Creative Commons Zero license</a>. If you use a photo <a href="/contact">I'd love to know!</a></p>
     </div>
     <div class="g-two-up g-center g-gutter-y-xl g-gutter-x-lg">
-        <?php while ( have_posts() ) : the_post(); ?>
+        <?php
+            $i = 0;
+            while ( have_posts() ) : the_post(); ?>
             <div>
                 <?php
             		$image_id = get_post_thumbnail_id();
@@ -23,14 +25,25 @@
                 <?php endif; ?>
                     <a href="<?php the_permalink(); ?>" class="u-ratio no-style" style="padding-top: <?php echo $ratio; ?>%;<?php if($color) { echo ' background-color:' . $color[0] . ';'; } ?>">
                         <?php if ( has_post_thumbnail() ) : ?>
-                            <img class="u-full"
-                                 src="<?php the_post_thumbnail_url('photography-xs'); ?>"
-                                 srcset="<?php the_post_thumbnail_url('photography-md'); ?> 800w,
-                                         <?php the_post_thumbnail_url('photography-sm'); ?> 600w,
-                                         <?php the_post_thumbnail_url('photography-xs'); ?> 400w"
-                                 sizes="(min-width: 35em) 41vw, 100vw"
-                                 alt="<?php the_title(); ?>"
-                                 title="<?php the_title(); ?>" />
+                            <?php if($i > 3) : ?>
+                                <img class="u-full"
+                                    data-original="<?php the_post_thumbnail_url('photography-xs'); ?>"
+                                    data-original-set="<?php the_post_thumbnail_url('photography-md'); ?> 800w,
+                                            <?php the_post_thumbnail_url('photography-sm'); ?> 600w,
+                                            <?php the_post_thumbnail_url('photography-xs'); ?> 400w"
+                                    sizes="(min-width: 35em) 41vw, 100vw"
+                                    alt="<?php the_title(); ?>"
+                                    title="<?php the_title(); ?>" />
+                            <?php else : ?>
+                                <img class="u-full"
+                                    src="<?php the_post_thumbnail_url('photography-xs'); ?>"
+                                    srcset="<?php the_post_thumbnail_url('photography-md'); ?> 800w,
+                                            <?php the_post_thumbnail_url('photography-sm'); ?> 600w,
+                                            <?php the_post_thumbnail_url('photography-xs'); ?> 400w"
+                                    sizes="(min-width: 35em) 41vw, 100vw"
+                                    alt="<?php the_title(); ?>"
+                                    title="<?php the_title(); ?>" />
+                            <?php endif; ?>
                         <?php endif; ?>
                     </a>
                 <?php if($height > $width) : ?>
@@ -38,7 +51,10 @@
                 <?php endif; ?>
                 <p class="h3 u-reset-type"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
             </div>
-        <?php endwhile; ?>
+        <?php 
+                $i++; 
+            endwhile;
+        ?>
     </div>
 <?php else : ?>
 	<div class="u-container">
--- footer.php
+++ footer.php
@@ -25,8 +25,11 @@
 
 	<?php wp_footer(); ?>
 
-	<script> window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date; ga('create', 'UA-727955-12', 'auto'); ga('send', 'pageview'); </script>
 	<script async src='<?php echo get_template_directory_uri(); ?>/build/js/analytics.js'></script>
+	<script> window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date; ga('create', 'UA-727955-12', 'auto'); ga('send', 'pageview'); </script>
+	<?php if(is_post_type_archive('photography')) : ?>
+		<script async src='<?php echo get_template_directory_uri(); ?>/build/js/lazyload.js'></script>
+	<?php endif; ?>
 
 </body>
 </html>
